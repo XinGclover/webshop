23a41fa0b588b344eff4cc737e454ac9739ba7af
sarko <sarko@192.168.0.18>
2019-04-17 11:49:26 +0200

    Test

--- src/java/inlog/Login.java
+++ src/java/inlog/Login.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package inlogg;
+package inlog;
 
 import DAO.LoginDAO;
 import javax.inject.Named;
