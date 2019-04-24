778ae81b84e54a4f264accd130845c94d73c5715
Carlos Lane <carlos.lane@yh.nackademin.se>
2019-04-24 11:41:20 +0200

    Refactored and cleaned up files

--- src/java/controller/LoginController.java
+++ src/java/controller/LoginController.java
@@ -5,93 +5,21 @@
  */
 package controller;
 
-import DAO.LoginDAO;
-import inloging.SessionUtils;
 import javax.inject.Named;
-import javax.enterprise.context.SessionScoped;
-import java.io.Serializable;
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
-import javax.servlet.http.HttpSession;
+import javax.enterprise.context.RequestScoped;
 
 /**
  *
- * @author sarko
+ * @author carlo
  */
-//@ManagedBean(name = "login", eager = true)
 @Named(value = "loginController")
-@SessionScoped
-public class LoginController implements Serializable {
+@RequestScoped
+public class LoginController {
 
-	private static final long serialVersionUID = 1094801825228386363L;
-	
-	private String pwd;
-	private String msg;
-	private String user;
-        
-        public LoginController(){};
-
-	public String getPwd() {
-		return pwd;
-	}
-
-	public void setPwd(String pwd) {
-		this.pwd = pwd;
-	}
-
-	public String getMsg() {
-		return msg;
-	}
-
-	public void setMsg(String msg) {
-		this.msg = msg;
-	}
-
-	public String getUser() {
-		return user;
-	}
-
-	public void setUser(String user) {
-		this.user = user;
-	}
-
-	//validate login
-	public String validateUsernamePassword() {
-		boolean valid = LoginDAO.validate(user, pwd);
-		if (valid) {
-			HttpSession session = SessionUtils.getSession();
-			session.setAttribute("username", user);
-			return "admin";
-		} else {
-			FacesContext.getCurrentInstance().addMessage(
-					null,
-					new FacesMessage(FacesMessage.SEVERITY_WARN,
-							"Incorrect Username and Passowrd",
-							"Please enter correct username and Password"));
-			return "login";
-		}
-	}
-        //test for inlog
-        public String CheckValidUser(){
-		if(user.equalsIgnoreCase("admin") && pwd.equalsIgnoreCase("admin")){
-			System.out.println("success");
-			return "success";
-		}
-		else{
-			return "fail";
-		}
-	}
-
-	//logout event, invalidate session
-	public String logout() {
-		HttpSession session = SessionUtils.getSession();
-		session.invalidate();
-		return "login";
-	}
-        
-        /*public String register() {
-		HttpSession session = SessionUtils.getSession();
-		session.invalidate();
-		return "success";
-	}*/
+    /**
+     * Creates a new instance of LoginController
+     */
+    public LoginController() {
+    }
+    
 }
