30269ce9625730af71fea2824cc637285b5c20ca
Carlos Lane <carlos.lane@yh.nackademin.se>
2019-04-26 15:32:22 +0200

    Functioning login for customer and admin against DB Redirect ready

--- java18-kvalit18-enterprise-webshop-ejb/src/java/crud/userManagementBean.java
+++ java18-kvalit18-enterprise-webshop-ejb/src/java/crud/userManagementBean.java
@@ -11,6 +11,7 @@ import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ejb.LocalBean;
 import javax.persistence.NoResultException;
+import jpa.Admins;
 import jpa.Customers;
 
 /**
@@ -19,58 +20,77 @@ import jpa.Customers;
  */
 @Stateless
 @LocalBean
-public class userManagementBean{
+public class userManagementBean {
+    
+    //user identifiers
+    private final int ADMIN = 1;
+    private final int CUSTOMER = 2;
 
     @EJB
     private GenericCrudService genericCrudServiceBean;
 
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
-
     public String login(String email, String password, boolean login) {
+
+        int userType = 0;   
+
+        Customers sessionCustomer = null;
+        Admins sessionAdmin = null;
+        
+           
+        
+        try {
+
+            //crudBean EJB method invocation
+            Map<String, Object> params = new HashMap<>();
+            params.put("email", email);
+
+            sessionCustomer = (Customers) genericCrudServiceBean.findWithNamedQuery("Customers.findByEmail", params).get(0);
+
+            userType = CUSTOMER;
+
+            System.out.println(sessionCustomer.toString());
+
+        } catch (NoResultException | NullPointerException | IndexOutOfBoundsException e1) {
+
+            try {
+
+                Map<String, Object> params = new HashMap<>();
+                params.put("email", email);
+                sessionAdmin = (Admins) genericCrudServiceBean.findWithNamedQuery("Admins.findByEmail", params).get(0);
+                userType = ADMIN;
+
+                System.out.println(sessionAdmin.toString());//change to 
+
+            } catch (NoResultException | NullPointerException | IndexOutOfBoundsException e2) {
+
+                System.out.println("There is no User matching that email ");
+            }
+
+        }
+
+        if (userType == CUSTOMER && sessionCustomer.getPassword().equals(password)) {
+
+            System.out.println("there is a customer with that name and password!");
+            return "store";
+
+        } else if (userType == ADMIN && sessionAdmin.getPassword().equals(password)) {
+            System.out.println("there is a Admin with that name and password!");
+             return "admin";
             
-                int userType = 0 ; 
-                
-                
-		boolean success = false;
 
-		Customers sessionCustomer = null;
-		try {
+        } else {
+            try {    //TEST PRINT DELETE BEFORE LAUNCH 
+                System.out.println("sesh: " + sessionCustomer.getPassword());
+                System.out.println("pwd: " + password);
+                System.out.println("Wrong Password");
 
-			//crudBean EJB method invocation
-			Map<String, Object> params = new HashMap<>();
-			params.put("email", email);
+            } catch (NullPointerException | IndexOutOfBoundsException e2) {
+                //catch for the test pring on the catch block 
+            }
+        }
+        return null;
 
-			sessionCustomer = (Customers) genericCrudServiceBean.findWithNamedQuery("Customers.findByEmail", params).get(0);
-
-			success = true;
-			System.out.println(sessionCustomer.toString());
-
- 		} catch (NoResultException | NullPointerException | IndexOutOfBoundsException e1) {
-                                        
-
-			System.out.println("There is no such Customer");
-		}
-
-		if (success && sessionCustomer.getPassword().equals(password)) {
-                    
-			System.out.println("there is a customer with that name and password!");
-                        return "store";
-
-		} else {
-                    try{
-			System.out.println("sesh: " + sessionCustomer.getPassword());
-			System.out.println("pwd: " + password);
-			System.out.println("Wrong Password");
-                    
-                    } catch(NullPointerException | IndexOutOfBoundsException e2){
-                        System.out.println("There is no such Customer with that email");
-                    }
-                }
-                return null;
-    
-    
-    
-     
     }
 }
