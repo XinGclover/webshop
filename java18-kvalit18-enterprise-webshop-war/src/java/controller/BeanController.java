d65aaa51aeade69b1f1bcd95a159cff19a6baf8c
nikalsh <niklas@helander.se>
2019-04-28 14:05:17 +0200

    gets cusotmer name on login

--- java18-kvalit18-enterprise-webshop-war/src/java/controller/BeanController.java
+++ java18-kvalit18-enterprise-webshop-war/src/java/controller/BeanController.java
@@ -5,12 +5,15 @@
  */
 package controller;
 
+import crud.GenericCrudService;
 import java.io.Serializable;
 import javax.ejb.EJB;
 import javax.inject.Named;
-import javax.enterprise.context.RequestScoped;
-import crud.userManagementBean; 
+import crud.userManagementBean;
+import java.util.HashMap;
+import java.util.Map;
 import javax.enterprise.context.SessionScoped;
+import jpa.Customers;
 
 /**
  *
@@ -20,82 +23,97 @@ import javax.enterprise.context.SessionScoped;
 @SessionScoped
 public class BeanController implements Serializable {
 
-        @EJB
-        private userManagementBean userManagementBean;
+	@EJB
+	private userManagementBean userManagementBean;
+	@EJB
+	private GenericCrudService crud;
 
-        private String email;
-        private String password;
-        private String confirmPassword;
-        private String firstName = "firstNamePlaceholder";
-        private String lastName = "lastNamePlaceholder";
-        private String address;
-        private boolean login; 
+	private String email;
+	private String password;
+	private String confirmPassword;
+	private String firstName = "firstNamePlaceholder";
+	private String lastName = "lastNamePlaceholder";
+	private String address;
+	private boolean login;
 
-        public BeanController() {
-        }
+	public BeanController() {
+	}
 
-        public String getEmail() {
-            return email;
-        }
+	public String getEmail() {
+		return email;
+	}
 
-        public void setEmail(String email) {
-            this.email = email;
-        }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-        public String getPassword() {
-            return password;
-        }
+	public String getPassword() {
+		return password;
+	}
 
-        public void setPassword(String password) {
-            this.password = password;
-        }
-        
-        public String getConfirmPassword() {
-            return confirmPassword;
-        }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-        public void setConfirmPassword(String confirmPassword) {
-            this.confirmPassword = confirmPassword;
-        }
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
 
-        public String getFirstName() {
-            return firstName;
-        }
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
 
-        public void setFirstName(String firstName) {
-            this.firstName = firstName;
-        }
+	public String getFirstName() {
+		return firstName;
+	}
 
-        public String getLastName() {
-            return lastName;
-        }
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
-        public void setLastName(String lastName) {
-            this.lastName = lastName;
-        }
+	public String getLastName() {
+		return lastName;
+	}
 
-        public String getAddress() {
-            return address;
-        }
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-        public void setAddress(String address) {
-            this.address = address;
-        }
-        
-        /**
-         * Sends information from the login to EJB layer for database check
-         * @return a string that is then used to redirect if success; 
-         **/
-        
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	/**
+	 * Sends information from the login to EJB layer for database check
+	 *
+	 * @return a string that is then used to redirect if success;
+	 *
+	 */
 	public String checkValidUser() {
-               
-           return userManagementBean.login(email, password,login); 
+		String redirect = userManagementBean.login(email, password, login);
 
+		if (redirect.equals("store")) {
+			setNames();
+		}
+
+		return redirect;
+	}
+
+	private void setNames() {
+		Map<String, Object> params = new HashMap<>();
+		params.put("email", email);
+		Customers c = (Customers) crud.findWithNamedQuery("Customers.findByEmail", params).get(0);
+		firstName = c.getFirstName();
+		lastName = c.getLastName();
 	}
 
 	//Registers the new user to the database 
 	public String registerCustomer() {
-            
-            return userManagementBean.register(firstName, lastName, email, address, password);
+
+		return userManagementBean.register(firstName, lastName, email, address, password);
 	}
 }
---
+++ java18-kvalit18-enterprise-webshop-war/src/java/controller/GenericDataTableDemo.java
@@ -0,0 +1,60 @@
+package controller;
+
+import crud.GenericCrudService;
+import java.io.Serializable;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import jpa.Customers;
+
+@Named(value = "genericTable")
+@SessionScoped
+public class GenericDataTableDemo implements Serializable {
+
+	@EJB
+	private GenericCrudService crud;
+
+	public GenericDataTableDemo() {
+	}
+	//pre requisite for generic data table generation in JSF
+	public List<List<String>> getContent() throws IllegalArgumentException, IllegalAccessException {
+		
+		List<List<String>> content = new ArrayList<>();
+		
+		for (Object e : crud.findWithNamedQuery("Customers.findAll")) {
+			
+			e = (Customers) e;
+			List<String> row = new ArrayList<>();
+			Field[] fields = e.getClass().getDeclaredFields();
+			
+			for (Field field : fields) {
+				
+				if (Modifier.isPrivate(field.getModifiers()) && !Modifier.isStatic(field.getModifiers())) {
+					row = new ArrayList<>();
+					try {
+						field.setAccessible(true);
+						System.out.println(field);
+						
+						//invoking private accesible true fields indirectly throws nullpointer
+						System.out.println(field.get(e).toString());
+						
+					} catch (IllegalArgumentException ex) {
+						Logger.getLogger(GenericDataTableDemo.class.getName()).log(Level.SEVERE, null, ex);
+					} catch (IllegalAccessException ex) {
+						Logger.getLogger(GenericDataTableDemo.class.getName()).log(Level.SEVERE, null, ex);
+					}
+				}
+			}
+			content.add(row);
+		}
+
+		return content;
+	}
+
+}
