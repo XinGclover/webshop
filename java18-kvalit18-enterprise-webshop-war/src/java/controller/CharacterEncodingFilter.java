afb9d55339b95a57d422c8472d30d7d596d56b37
Carlos Lane <carlos.lane@yh.nackademin.se>
2019-05-07 09:33:48 +0200

    merged and implemented abstract Methods in charachterencodingfilter.java

--- java18-kvalit18-enterprise-webshop-war/src/java/controller/CharacterEncodingFilter.java
+++ java18-kvalit18-enterprise-webshop-war/src/java/controller/CharacterEncodingFilter.java
@@ -3,6 +3,7 @@ package controller;
 import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -17,5 +18,15 @@ public class CharacterEncodingFilter implements Filter {
 		chain.doFilter(request, response);
 
 	}
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void destroy() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
 }
